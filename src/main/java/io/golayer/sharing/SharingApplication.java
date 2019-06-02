8f73d32fad368b88a11ca389cd89f0eeeaec9199
rublin <toor.ua@gmail.com>
2019-06-02 11:19:40 +0300

    readme + swagger

--- src/main/java/io/golayer/sharing/SharingApplication.java
+++ src/main/java/io/golayer/sharing/SharingApplication.java
@@ -2,11 +2,45 @@ package io.golayer.sharing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
 @SpringBootApplication
 public class SharingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SharingApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfo(
+                "Sharing service",
+                "Layer Backend Engineer Test",
+                "0.1",
+                null,
+                null,
+                null,
+                null,
+                Collections.emptyList());
     }
 }
