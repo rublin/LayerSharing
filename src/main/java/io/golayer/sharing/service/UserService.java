97e82cee8357c806b40f686017c5922620bf08b1
rublin <toor.ua@gmail.com>
2019-06-02 14:44:24 +0300

    remove unused methods

--- src/main/java/io/golayer/sharing/service/UserService.java
+++ src/main/java/io/golayer/sharing/service/UserService.java
@@ -2,12 +2,6 @@ package io.golayer.sharing.service;
 
 import io.golayer.sharing.model.User;
 
-import java.util.List;
-
 public interface UserService {
-    List<User> getAll();
-
-    User create(User user);
-
     User findOrCreate(String email);
 }
--- src/main/java/io/golayer/sharing/service/UserServiceImpl.java
+++ src/main/java/io/golayer/sharing/service/UserServiceImpl.java
@@ -1,12 +1,10 @@
 package io.golayer.sharing.service;
 
-import com.google.common.collect.Lists;
 import io.golayer.sharing.model.User;
 import io.golayer.sharing.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,16 +12,6 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-
-    @Override
-    public List<User> getAll() {
-        return Lists.newArrayList(userRepository.findAll());
-    }
-
-    @Override
-    public User create(User user) {
-        return userRepository.save(user);
-    }
 
     @Override
     public User findOrCreate(String email) {
